e0480160bd646e0dfb55f33d0a6e553efb4ce56a
Vinicius01br <viniciuslima1809@hotmail.com>
2024-05-02 15:29:02 -0300

    js formulario cep

---
+++ js/contato.js
@@ -0,0 +1,19 @@
+`use strict`
+/* Selecionar os Elementos que serão manipulados*/
+const formulario = document.querySelector("form")
+const campoCep = formulario . querySelector("#cep")
+const campoEndereco = formulario . querySelector("#endereco")
+const campoBairro = formulario . querySelector("#bairro")
+const campoCidade = formulario . querySelector("#cidade")
+const campoEstado = formulario . querySelector("#estado")
+const botaoBuscar = formulario . querySelector("#buscar")
+const mensagem = formulario . querySelector("#status")
+
+/*detectando quando o botão de buscar CEP é adicionado */
+botaoBuscar.addEventListener("click", function(event){
+    /* Anular o comportamento padrão de redirecionamento/recarregamento da pagína.sempre acontece ao trabalhar
+    com <a> e <form>.*/
+    event.preventDefault();
+    
+
+});
